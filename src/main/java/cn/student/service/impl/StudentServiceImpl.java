e257bd4228b7a323cc0dae7be0fe3066802c3363
1260487587 <1260487587@qq.com>
2019-01-15 18:54:47 +0800

    the Second push

--- src/main/java/cn/student/service/impl/StudentServiceImpl.java
+++ src/main/java/cn/student/service/impl/StudentServiceImpl.java
@@ -51,4 +51,8 @@ public class StudentServiceImpl implements IStudentService {
     public int checkStuName(Student student) throws Exception {
         return dao.addStudent(student);
     }
+
+    public void test(){
+
+    }
 }
